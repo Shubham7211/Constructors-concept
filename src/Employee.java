d35bf8b361be5d925ea1b6ce0e43b82786444f79
Shubham7211 <shubhamundrepatil77@gmail.com>
2021-09-19 14:44:32 +0530

    new practice problems

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+import java.util.jar.Attributes.Name;
+
+public class Employee {
+	
+	String name="shubham undre";
+	int empid=7211;
+	public static void main(String[] args) {
+
+		Employee shubham=new Employee();
+		System.out.println(shubham.name);
+	}
+
+}
